320edc52fcaee8d570c201a78b42c45b143076d8
Vishwajeet Muthe <vsmuthe1970@yahoo.com>
2025-05-08 13:26:12 +0530

    Request Body complete

--- main.py
+++ main.py
@@ -81,11 +81,33 @@ async def get_user_item(user_id: int, item_id: str, q: str | None = None, short:
 
 class Item(BaseModel):
     name: str
-    description: str
+    description: str | None = None
     price: float
-    tax: float
+    tax: float | None = None
 
 
 @app.post("/items")
-async def create_item():
-    retrun items
+async def create_item(item: Item):
+    item_dict = item.dict()
+
+    if item.tax:
+        item.tax = (item.tax * item.price)/100
+        price_with_tax = item.price + item.tax
+        item_dict.update({"price_with_tax": price_with_tax})
+    return item_dict
+
+# PUT
+@app.put("/items/{item_id}")
+async def create_item_with_put(item_id: int, item: Item, q : str | None = None):
+    result = {"item_id": item_id, **item.dict()}
+
+    if q:
+        result.update({"q": q})
+    return result
+
+@app.get("/items")
+async def read_items(q: str | None = None):
+    result = {"items": [{"item_id": "Foo", "item_id": "Bar"}]}
+    if q:
+        result.update({"q": q})
+    return result
